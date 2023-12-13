402066433127662ef6b90fbb3451ce096e114bd7
NagendraVarma1 <nagendravarmal512@gmail.com>
2023-12-13 10:13:59 +0530

    task-15

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Veera@512'
+})
+
+module.exports = pool.promise();
